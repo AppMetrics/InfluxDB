2acb4d77cdf79882d9e4fdf70113f26118e17944
Thomas Bolon <tbolon@mb3m.fr>
2019-02-05 11:38:04 +0100

    add base project

---
+++ benchmarks/App.Metrics.InfluxDB.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App.Metrics.InfluxDB.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
